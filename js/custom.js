2d6bfadb1711c965bacbad88b7913d1adcd7d763
unknown <kasra.parhun@gmail.com>
2020-07-29 19:29:49 +0430

    some practices over errors

--- js/custom.js
+++ js/custom.js
@@ -1,60 +1,39 @@
-// function loadFunction(){
-//   console.log('The dom has been loaded');
-// }
-//
-// document.addEventListener('DOMContentLoaded', loadFunction);
-//
-//
-// function assignListeneres(){
-//   let divs = document.getElementsByTagName('div');
-//   for (let i = 0; i < divs.length; i++){
-//     divs[i].addEventListener('click', showID, true);
-//     divs[i].addEventListener('click', showID, false);
-//   }
-// }
-//
-//
-// function showID(evt){
-//   console.log(evt);
-//   alert(evt.currentTarget.id);
-// }
+function onSuccess(){
+  console.log('you are correct');
+}
 
-// document.addEventListener('DOMContentLoaded', assignListeneres, false);
+function onError(e){
+  console.log(e.message);
+}
 
-
-// let myEvent = new Event('finished');
-// let myAnotherEvent = new CustomEvent('done', {'detail': 'done looping!'});
-//
-// document.addEventListener('finished', function(evt){
-//   console.log("finished event called");
-// });
-//
-// document.addEventListener('done', function(evt){
-//   console.log(evt.detail);
-// });
-//
-// for (let j = 0; j < 100; j++){
-//   if (j == 99){
-//     document.dispatchEvent(myEvent);
-//     document.dispatchEvent(myAnotherEvent);
-//   }
-// }
-
-
-let textField = document.getElementsByTagName("input");
-let isListening = true;
-
-function changeMessage(e){
-  if (isListening){
-    isListening = !isListening;
-    document.removeEventListener('click', changeMessage);
-    textField[0].value = 'Event Listener = ' + isListening;
+function isFirstOfficer(name, onError, onSuccess){
+  if (name === 'Spock'){
+    onSuccess();
+  } else {
+    onError(new Error('Sorry, Wrong officer'));
   }
 }
 
-function setupDoc(evt){
-  textField[0].value = 'eventListener = true';
-  document.addEventListener('click', changeMessage);
+
+isFirstOfficer('scotty', onError, onSuccess);
+isFirstOfficer('Spock', onError, onSuccess);
+
+let refErr = new ReferenceError('This is a refrence error');
+console.log(refErr.message);
+
+class myCustomError extends Error{
+  constructor(message){
+    super(message);
+  }
 }
 
-document.addEventListener('DOMContentLoaded', setupDoc);
+let myCustomErrorInstance = new myCustomError('this is a custom error');
+console.log(myCustomErrorInstance.message);
+console.log(myCustomErrorInstance.stack);
+
+try{
+  throw(new myCustomError('there has beeeen a mistake!'));
+} catch(e) {
+  console.log(e.message);
+  console.log(e.stack);
+}
