170210998d9e5de0d0611a50d438708b36a74472
unknown <kasra.parhun@gmail.com>
2020-07-30 18:34:32 +0430

    some practices over regular expression

--- js/custom.js
+++ js/custom.js
@@ -1,39 +1,63 @@
-function onSuccess(){
-  console.log('you are correct');
-}
-
-function onError(e){
-  console.log(e.message);
-}
-
-function isFirstOfficer(name, onError, onSuccess){
-  if (name === 'Spock'){
-    onSuccess();
-  } else {
-    onError(new Error('Sorry, Wrong officer'));
-  }
-}
+// let companyBio = 'Twitter is an online social ...';
+// let simplePattern = /(twitter)/gi;
+// let simplePatternConstructor = new RegExp(simplePattern);
+// console.log(simplePatternConstructor.exec(companyBio));
 
 
-isFirstOfficer('scotty', onError, onSuccess);
-isFirstOfficer('Spock', onError, onSuccess);
+// let words = `Mofth mon darth`;
+// words += `\nMouth mouse mouth`;
+//
+// let multilineExpression = /mon | mouse/gi;
+//
+// let multiLineResult = multilineExpression.exec(words);
+//
+// console.log(multiLineResult);
+//
+//
+// let groupOfWords = 'cats, bats, dogs, logs, cogs';
+// let groupRegX = /[cb]ats|[dl]og/;
+// console.log(groupRegX.exec(groupOfWords));
+//
+// let textWithNumbers = 'USS Enterprise 1701-D';
+// let searchNumbers = /[0-9]/;
+// let searchNumbersGreedy = /[0-9]+/;
+//
+// console.log(searchNumbers.exec(textWithNumbers));
+// console.log(searchNumbersGreedy.exec(textWithNumbers));
 
-let refErr = new ReferenceError('This is a refrence error');
-console.log(refErr.message);
 
-class myCustomError extends Error{
-  constructor(message){
-    super(message);
-  }
-}
+// let startAnchor = /^M/;
+// let multiLineAnchor = /^M/m;
+// let endOfLineAnchor = /binks.$/gm;
+// let firstInstanceAnchor = /\ar/;
+// let startOrEndAnchor = /^darth|binks.$/;
+//
+// console.log(startAnchor.exec(ipsumString));
+// console.log(multiLineAnchor.exec(ipsumString));
+// console.log(endOfLineAnchor.exec(ipsumString));
+// console.log(firstInstanceAnchor.exec(ipsumString));
+// console.log(startOrEndAnchor.exec(ipsumString));
 
-let myCustomErrorInstance = new myCustomError('this is a custom error');
-console.log(myCustomErrorInstance.message);
-console.log(myCustomErrorInstance.stack);
+let ipsumString = 'Moff mon darth solo jabba yavin darth. Skywalker endor k-3po mon fett binks.';
+ipsumString += 'Moff mon darth solo jabba yavin darth. Skywalker endor k-3po mon fett binks. file_record_transcript.pdf';
 
-try{
-  throw(new myCustomError('there has beeeen a mistake!'));
-} catch(e) {
-  console.log(e.message);
-  console.log(e.stack);
-}
+let groupOfFilesNoExtension = /(.*)\.pdf/;
+let gropuOfFilesWithExtension = /(.*)\pdf/;
+
+console.log(groupOfFilesNoExtension.exec(ipsumString));
+console.log(gropuOfFilesWithExtension.exec(ipsumString));
+
+
+
+
+// let greedyQuantifier = /b+/;
+// console.log(greedyQuantifier.exec(ipsumString));
+//
+// let docileQuantifier = /.*k-/;
+// console.log(docileQuantifier.exec(ipsumString));
+//
+// let lazyQuantifier = /jabba*?/;
+// console.log(lazyQuantifier.exec(ipsumString));
+//
+// let helpfulQuantifier = /.*?yavin/;
+// console.log(helpfulQuantifier.exec(ipsumString));
