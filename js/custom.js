0d223254bd4402e43877028bec5c9fa01a00eb6a
unknown <kasra.parhun@gmail.com>
2020-08-05 19:12:02 +0430

    some practices with reduce

--- js/custom.js
+++ js/custom.js
@@ -1,63 +1,39 @@
-// let companyBio = 'Twitter is an online social ...';
-// let simplePattern = /(twitter)/gi;
-// let simplePatternConstructor = new RegExp(simplePattern);
-// console.log(simplePatternConstructor.exec(companyBio));
+// function sum(numbers){
+//   return numbers.reduce((accumulator, currentElement) => {
+//     return accumulator + currentElement;
+//   }, 0);
+// };
+//
+// console.log(sum([4,5]));
 
 
-// let words = `Mofth mon darth`;
-// words += `\nMouth mouse mouth`;
+// function sum(number){
+//   return number.reduce((acc, cI) => {
+//     return acc + cI;
+//   }, 0);
+// }
 //
-// let multilineExpression = /mon | mouse/gi;
-//
-// let multiLineResult = multilineExpression.exec(words);
-//
-// console.log(multiLineResult);
-//
-//
-// let groupOfWords = 'cats, bats, dogs, logs, cogs';
-// let groupRegX = /[cb]ats|[dl]og/;
-// console.log(groupRegX.exec(groupOfWords));
-//
-// let textWithNumbers = 'USS Enterprise 1701-D';
-// let searchNumbers = /[0-9]/;
-// let searchNumbersGreedy = /[0-9]+/;
-//
-// console.log(searchNumbers.exec(textWithNumbers));
-// console.log(searchNumbersGreedy.exec(textWithNumbers));
+// console.log(sum([2,3]));
 
 
-// let startAnchor = /^M/;
-// let multiLineAnchor = /^M/m;
-// let endOfLineAnchor = /binks.$/gm;
-// let firstInstanceAnchor = /\ar/;
-// let startOrEndAnchor = /^darth|binks.$/;
-//
-// console.log(startAnchor.exec(ipsumString));
-// console.log(multiLineAnchor.exec(ipsumString));
-// console.log(endOfLineAnchor.exec(ipsumString));
-// console.log(firstInstanceAnchor.exec(ipsumString));
-// console.log(startOrEndAnchor.exec(ipsumString));
+function sum(numbers){
+  return numbers.reduce((sum, nextNum) => sum + nextNum, 0)
+}
 
-let ipsumString = 'Moff mon darth solo jabba yavin darth. Skywalker endor k-3po mon fett binks.';
-ipsumString += 'Moff mon darth solo jabba yavin darth. Skywalker endor k-3po mon fett binks. file_record_transcript.pdf';
-
-let groupOfFilesNoExtension = /(.*)\.pdf/;
-let gropuOfFilesWithExtension = /(.*)\pdf/;
-
-console.log(groupOfFilesNoExtension.exec(ipsumString));
-console.log(gropuOfFilesWithExtension.exec(ipsumString));
+console.log(sum([4,5]));
 
 
+function addOneToEach(numbers){
+  return numbers.map(num => ++num);
+}
+
+console.log(addOneToEach([2,3]));
 
 
-// let greedyQuantifier = /b+/;
-// console.log(greedyQuantifier.exec(ipsumString));
-//
-// let docileQuantifier = /.*k-/;
-// console.log(docileQuantifier.exec(ipsumString));
-//
-// let lazyQuantifier = /jabba*?/;
-// console.log(lazyQuantifier.exec(ipsumString));
-//
-// let helpfulQuantifier = /.*?yavin/;
-// console.log(helpfulQuantifier.exec(ipsumString));
+Array.prototype.map = function(mapper){
+  return this.reduce((acc, cI) => {
+    const mappedCurrentIndex = mapper(currentElement);
+    acc.push(mappedCurrentIndex)
+    return acc;
+  })
+}
