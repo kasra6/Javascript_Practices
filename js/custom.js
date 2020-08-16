5191440416433140898a0c2f2a7d36f922bd5a61
unknown <kasra.parhun@gmail.com>
2020-08-16 21:45:33 +0430

    some tests applied

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,44 @@
+function lastnameError(){
+  const lastnameAlert = document.querySelector("span.lastname-alert");
+  console.log(lastnameAlert);
+
+  lastnameAlert.innerHTML = "Please write your Last Name";
+  console.log(lastnameAlert.textContent);
+  console.log(lastnameAlert);
+  lastnameInput.focus();
+}
+
+function validate(e){
+  e.preventDefault();
+
+  // checking the input boxes to not be blank
+  // if (nameInput.value === ''){
+  //   nameError();
+  // }
+
+  if (lastnameInput.value === ''){
+    console.log("lastname");
+    lastnameError();
+  }
+
+  // if (phonenumInput.value === ''){
+  //   phonenumError();
+  // }
+  //
+  // if (emailInput.value === ''){
+  //   emailError();
+  //   return false;
+  // }
+
+  return true;
+}
+
+const lastnameInput = document.querySelector("input[name=lastname]");
+
+const form = document.querySelector("input[type=submit]");
+
+form.addEventListener('click', validate);
+
 // function sum(numbers){
 //   return numbers.reduce((accumulator, currentElement) => {
 //     return accumulator + currentElement;
@@ -16,24 +57,24 @@
 // console.log(sum([2,3]));
 
 
-function sum(numbers){
-  return numbers.reduce((sum, nextNum) => sum + nextNum, 0)
-}
-
-console.log(sum([4,5]));
-
-
-function addOneToEach(numbers){
-  return numbers.map(num => ++num);
-}
-
-console.log(addOneToEach([2,3]));
-
-
-Array.prototype.map = function(mapper){
-  return this.reduce((acc, cI) => {
-    const mappedCurrentIndex = mapper(currentElement);
-    acc.push(mappedCurrentIndex)
-    return acc;
-  })
-}
+// function sum(numbers){
+//   return numbers.reduce((sum, nextNum) => sum + nextNum, 0)
+// }
+//
+// console.log(sum([4,5]));
+//
+//
+// function addOneToEach(numbers){
+//   return numbers.map(num => ++num);
+// }
+//
+// console.log(addOneToEach([2,3]));
+//
+//
+// Array.prototype.map = function(mapper){
+//   return this.reduce((acc, cI) => {
+//     const mappedCurrentIndex = mapper(currentElement);
+//     acc.push(mappedCurrentIndex)
+//     return acc;
+//   })
+// }
